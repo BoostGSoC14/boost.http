a9cc6838afd1672d794b8c9bef3a0dadd3950c8e
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-05-21 23:03:52 -0300

    Finished the definition of the easiest socket concepts' requirements.

--- include/boost/http/traits.hpp
+++ include/boost/http/traits.hpp
@@ -25,14 +25,73 @@ struct has_outgoing_state
   : std::true_type
 {};
 
+template<typename T, typename Enable = std::true_type>
+struct has_outgoing_response_native_stream: std::false_type
+{};
+
+template<typename T>
+struct has_outgoing_response_native_stream
+<T, typename std::is_convertible<decltype(static_cast<T const*>(nullptr)
+                                          ->outgoing_response_native_stream()),
+                                 bool>::type>
+  : std::true_type
+{};
+
+template<typename T, typename Enable = std::true_type>
+struct has_incoming_request_continue_required: std::false_type
+{};
+
+template<typename T>
+struct has_incoming_request_continue_required
+<T, typename std::is_convertible<decltype(static_cast<T const*>(nullptr)
+                                          ->incoming_request_continue_required()),
+                                 bool>::type>
+  : std::true_type
+{};
+
+template<typename T, typename Enable = std::true_type>
+struct has_incoming_request_upgrade_required: std::false_type
+{};
+
+template<typename T>
+struct has_incoming_request_upgrade_required
+<T, typename std::is_convertible<decltype(static_cast<T const*>(nullptr)
+                                          ->incoming_request_upgrade_required()),
+                                 bool>::type>
+  : std::true_type
+{};
+
+template<typename T, typename Enable = std::true_type>
+struct has_outgoing_response_write_continue : std::false_type
+{};
+
+template<typename T>
+struct has_outgoing_response_write_continue
+<T, typename std::is_convertible<decltype(static_cast<T*>(nullptr)
+                                          ->outgoing_response_write_continue()),
+                                 bool>::type>
+  : std::true_type
+{};
+
 template<class T>
 constexpr bool is_http_socket_helper()
 {
-    return is_message<T>::value && has_outgoing_state<T>::value;
+    return is_message<T>::value && has_outgoing_state<T>::value
+        && has_outgoing_response_native_stream<T>::value
+        && has_incoming_request_continue_required<T>::value
+        && has_incoming_request_upgrade_required<T>::value
+        && has_outgoing_response_write_continue<T>::value;
 }
 
 } // namespace detail
 
+/**
+ * List of missing tests:
+ *
+ * - Check if member message_type exists and is fullfill the message concept,
+ *   although this type may end up being probably a concrete type opposed to a
+ *   template.
+ */
 template<class T>
 struct is_socket
     : std::integral_constant<bool, detail::is_http_socket_helper<T>()>
--- src/tests/socket_concept.cpp
+++ src/tests/socket_concept.cpp
@@ -10,6 +10,10 @@ class A
 struct B
 {
     boost::http::outgoing_state outgoing_state() const;
+    bool outgoing_response_native_stream() const;
+    bool incoming_request_continue_required() const;
+    bool incoming_request_upgrade_required() const;
+    bool outgoing_response_write_continue();
 };
 
 struct C: B
@@ -19,11 +23,19 @@ struct C: B
 struct D
 {
     int outgoing_state() const;
+    void outgoing_response_native_stream() const;
+    void incoming_request_continue_required() const;
+    void incoming_request_upgrade_required() const;
+    void outgoing_response_write_continue();
 };
 
 struct E
 {
     boost::http::outgoing_state outgoing_state();
+    bool outgoing_response_native_stream();
+    bool incoming_request_continue_required();
+    bool incoming_request_upgrade_required();
+    bool outgoing_response_write_continue() const; //< kind of useless
 };
 
 struct F
@@ -33,7 +45,16 @@ struct F
         operator boost::http::outgoing_state();
     };
 
+    struct Y
+    {
+        operator bool();
+    };
+
     X outgoing_state() const;
+    Y outgoing_response_native_stream() const;
+    Y incoming_request_continue_required() const;
+    Y incoming_request_upgrade_required() const;
+    Y outgoing_response_write_continue();
 };
 
 BOOST_AUTO_TEST_CASE(Simple_attributes) {
