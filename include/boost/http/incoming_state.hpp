1551c88c5b6ccd6e0c8d0e986b90f141cdd8b22b
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-06-27 04:48:56 -0300

    A few clarifications on the doc of `incoming_state`.

--- include/boost/http/incoming_state.hpp
+++ include/boost/http/incoming_state.hpp
@@ -34,7 +34,7 @@ enum class incoming_state
      * An easy to interpret this state is that it is complete for the previous
      * message and empty for the next one.
      *
-     * At this state, you can only issue a `read_message` action.
+     * At this state, you can only issue a `receive_message` action.
      */
     empty,
     /**
@@ -43,7 +43,7 @@ enum class incoming_state
      *
      * No more `receive_message` actions can be issued from this state.
      *
-     * From this state, you can issue the `read_some_body` action. The state
+     * From this state, you can issue the `receive_some_body` action. The state
      * will change to `body_ready` once all body was read. In streaming
      * connections (e.g. HTTP/1.1 chunked entities), this condition (body fully
      * received) might never happen.
@@ -57,10 +57,6 @@ enum class incoming_state
      *
      * From this state, you can only issue the `receive_trailers` action.
      *
-     * The http producer has the freedom to make multiple state changes within
-     * one action (e.g. user issue `receive_message` and state is changed to
-     * body_ready at the time the handler is called).
-     *
      * Once this state is reached, you can safely assume that no more body parts
      * will be received.
      */
@@ -68,7 +64,9 @@ enum class incoming_state
     /**
      * Only possible in incoming response mode. It means the message is complete
      * and you can no longer issue another `receive_message` until something
-     * else is done (e.g. send another http request).
+     * else is done (e.g. send another http request). This is a
+     * different/special value, because the "something else to do" might not be
+     * related to read actions.
      *
      * It can be reached from `body_ready` state, after all trailers have been
      * received. It's safe to assume that all message data is available at the
