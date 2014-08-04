4c3d791aa7ff7702ec21ec4a5a97839fecc79c5a
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-08-04 02:19:17 -0300

    add documentation structure skel

---
+++ doc/extraheaders/httpnamespace.hpp
@@ -0,0 +1,10 @@
+namespace boost {
+
+/**
+ * This namespace holds all documentation related to the Boost.Http lib and no
+ * other namespace is used.
+ */
+namespace http {
+} // namespace http
+
+} // namespace boost
