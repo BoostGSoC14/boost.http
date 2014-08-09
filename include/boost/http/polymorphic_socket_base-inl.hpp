48ccc9f921f461dea79ed85c2d6c976bce8c4f4c
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-08-09 01:37:57 -0300

    add polymorphic_socket_base interface

---
+++ include/boost/http/polymorphic_socket_base-inl.hpp
@@ -0,0 +1,105 @@
+/* Copyright (c) 2014 Vinícius dos Santos Oliveira
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
+namespace boost {
+namespace http {
+
+template<class CompletionToken>
+typename asio::async_result<
+    typename asio::handler_type<CompletionToken,
+                                void(system::error_code)>::type>::type
+polymorphic_socket_base
+::async_read_some(message &message, CompletionToken &&token)
+{
+    typedef typename asio::handler_type<
+        CompletionToken, void(system::error_code)>::type Handler;
+
+    Handler handler(std::forward<CompletionToken>(token));
+
+    asio::async_result<Handler> result(handler);
+
+    async_read_some(message, callback_type(handler));
+
+    return result.get();
+}
+
+template<class CompletionToken>
+typename asio::async_result<
+    typename asio::handler_type<CompletionToken,
+                                void(system::error_code)>::type>::type
+polymorphic_socket_base
+::async_read_trailers(message &message, CompletionToken &&token)
+{
+    typedef typename asio::handler_type<
+        CompletionToken, void(system::error_code)>::type Handler;
+
+    Handler handler(std::forward<CompletionToken>(token));
+
+    asio::async_result<Handler> result(handler);
+
+    async_read_trailers(message, callback_type(handler));
+
+    return result.get();
+}
+
+template<class CompletionToken>
+typename asio::async_result<
+    typename asio::handler_type<CompletionToken,
+                                void(system::error_code)>::type>::type
+polymorphic_socket_base
+::async_write(message &message, CompletionToken &&token)
+{
+    typedef typename asio::handler_type<
+        CompletionToken, void(system::error_code)>::type Handler;
+
+    Handler handler(std::forward<CompletionToken>(token));
+
+    asio::async_result<Handler> result(handler);
+
+    async_write(message, callback_type(handler));
+
+    return result.get();
+}
+
+template<class CompletionToken>
+typename asio::async_result<
+    typename asio::handler_type<CompletionToken,
+                                void(system::error_code)>::type>::type
+polymorphic_socket_base
+::async_write_trailers(message &message, CompletionToken &&token)
+{
+    typedef typename asio::handler_type<
+        CompletionToken, void(system::error_code)>::type Handler;
+
+    Handler handler(std::forward<CompletionToken>(token));
+
+    asio::async_result<Handler> result(handler);
+
+    async_write_trailers(message, callback_type(handler));
+
+    return result.get();
+}
+
+template<class CompletionToken>
+typename asio::async_result<
+    typename asio::handler_type<CompletionToken,
+                                void(system::error_code)>::type>::type
+polymorphic_socket_base
+::async_write_end_of_message(CompletionToken &&token)
+{
+    typedef typename asio::handler_type<
+        CompletionToken, void(system::error_code)>::type Handler;
+
+    Handler handler(std::forward<CompletionToken>(token));
+
+    asio::async_result<Handler> result(handler);
+
+    async_write_end_of_message(callback_type(handler));
+
+    return result.get();
+}
+
+} // namespace http
+} // namespace boost
---
+++ include/boost/http/polymorphic_socket_base.hpp
@@ -0,0 +1,76 @@
+/* Copyright (c) 2014 Vinícius dos Santos Oliveira
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt) */
+
+#ifndef BOOST_HTTP_POLYMORPHIC_SOCKET_BASE_H
+#define BOOST_HTTP_POLYMORPHIC_SOCKET_BASE_H
+
+#include <functional>
+
+#include <boost/system/error_code.hpp>
+
+#include <boost/http/read_state.hpp>
+#include <boost/http/write_state.hpp>
+#include <boost/http/message.hpp>
+
+namespace boost {
+namespace http {
+
+class polymorphic_socket_base
+{
+public:
+    typedef std::function<void(system::error_code)> callback_type;
+
+    // ### ABI-stable interface ###
+    virtual http::read_state read_state() const = 0;
+    virtual http::write_state write_state() const = 0;
+    virtual void async_read_some(message &message, callback_type handler) = 0;
+    virtual void async_read_trailers(message &message,
+                                     callback_type handler) = 0;
+    virtual void async_write(const message &message,
+                             callback_type handler) = 0;
+    virtual void async_write_trailers(const message &message,
+                                      callback_type handler) = 0;
+    virtual void async_write_end_of_message(callback_type handler) = 0;
+
+    virtual ~polymorphic_socket_base() = default;
+
+    // ### wrappers for the ASIO's extensible model ###
+    template<class CompletionToken>
+    typename asio::async_result<
+        typename asio::handler_type<CompletionToken,
+                                    void(system::error_code)>::type>::type
+    async_read_some(message &message, CompletionToken &&token);
+
+    template<class CompletionToken>
+    typename asio::async_result<
+        typename asio::handler_type<CompletionToken,
+                                    void(system::error_code)>::type>::type
+    async_read_trailers(message &message, CompletionToken &&token);
+
+    template<class CompletionToken>
+    typename asio::async_result<
+        typename asio::handler_type<CompletionToken,
+                                    void(system::error_code)>::type>::type
+    async_write(message &message, CompletionToken &&token);
+
+    template<class CompletionToken>
+    typename asio::async_result<
+        typename asio::handler_type<CompletionToken,
+                                    void(system::error_code)>::type>::type
+    async_write_trailers(message &message, CompletionToken &&token);
+
+    template<class CompletionToken>
+    typename asio::async_result<
+        typename asio::handler_type<CompletionToken,
+                                    void(system::error_code)>::type>::type
+    async_write_end_of_message(CompletionToken &&token);
+};
+
+} // namespace http
+} // namespace boost
+
+#include "polymorphic_socket_base-inl.hpp"
+
+#endif // BOOST_HTTP_POLYMORPHIC_SOCKET_BASE_H
