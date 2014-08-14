702cfaa19773bc5c57364e5a0d4e6e51f2a64a8e
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2014-08-14 05:35:51 -0300

    add http date conversion utilities

--- include/boost/http/algorithm/header.hpp
+++ include/boost/http/algorithm/header.hpp
@@ -9,11 +9,237 @@
 #include <cctype>
 
 #include <algorithm>
+#include <regex>
+#include <type_traits>
 
 #include <boost/utility/string_ref.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 
 namespace boost {
 namespace http {
+
+namespace detail {
+
+/* This function was created, because most int <-> string conversion
+ * techniques/abstractions can only handle char* or basic_string. They don't
+ * support generic strings.
+ *
+ * Despite this being the critical point to motivate this development, there are
+ * other disappointing behaviours with the found abstractions. For instance, I
+ * invested a big effort to allocate as little memory on the heap as I could,
+ * but most of the abstractions will just use something like standard library
+ * string-based stream or other expensive abstractions.
+ *
+ * However, I'm not advocating the interface developed here is better. This
+ * interface is limited to non-negative base-10 conversions and don't properly
+ * handle Unicode characters. Even worse, it doesn't check for invalid
+ * input.
+ *
+ * Don't freak out about the lack of valid inputs. This is checked by the
+ * regex layer.
+ *
+ * It's just a workaround to not throw away all the efforts spent in the other
+ * layers.*/
+template<class Target, class BidirIt>
+Target from_decimal_string(BidirIt begin, BidirIt end)
+{
+    static_assert(std::is_same<
+                    typename std::iterator_traits<BidirIt>::value_type,
+                    char>::value,
+                  "from_decimal_string only supports char type");
+    Target ret = 0, digit = 1;
+    std::reverse_iterator<BidirIt> it(end), rend(begin);
+    for (;it != rend;++it) {
+        ret += (*it - '0') * digit;
+        digit *= 10;
+    }
+    return ret;
+}
+
+template<class Target, class BidirIt>
+Target from_decimal_submatch(const std::sub_match<BidirIt> &match)
+{
+    return from_decimal_string<Target>(match.first, match.second);
+}
+
+template<class Target, class BidirIt>
+Target from_submatch_to_month(std::sub_match<BidirIt> m)
+{
+    switch (*m.first) {
+    case 'A': // "Apr" or "Aug"
+        ++m.first;
+        return (*m.first == 'p') ? 4 : 8;
+    case 'D': // "Dec"
+        return 12;
+    case 'F': // "Feb"
+        return 2;
+    case 'J': // "Jan" or "Jun" or "Jul"
+        ++m.first;
+        if (*m.first == 'a')
+            return 1;
+        ++m.first;
+        return (*m.first == 'n') ? 6 : 7;
+    case 'M': // "Mar" or "May"
+        ++m.first; ++m.first;
+        return (*m.first == 'r') ? 3 : 5;
+    case 'N': // "Nov"
+        return 11;
+    case 'O': // "Oct"
+        return 10;
+    case 'S': // "Sep"
+        return 9;
+    default:
+        assert(false);
+    }
+}
+
+template<class String>
+bool rfc1123(const String &value, posix_time::ptime &datetime)
+{
+    using namespace gregorian;
+    using namespace posix_time;
+
+    typedef date::year_type::value_type year_type;
+    typedef date::month_type::value_type month_type;
+    typedef date::day_type::value_type day_type;
+    typedef time_duration::hour_type hour_type;
+    typedef time_duration::min_type min_type;
+    typedef time_duration::sec_type sec_type;
+
+    static const std::basic_regex<typename String::value_type>
+        regex("(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun), " // day
+              "(\\d{2}) " // day-1
+              "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) " // month-2
+              "(\\d{4}) " // year-3
+              "(\\d{2}):" // hour-4
+              "(\\d{2}):" // minutes-5
+              "(\\d{2}) " // seconds-6
+              "GMT");
+
+    std::match_results<typename String::const_iterator> matches;
+    if (!std::regex_match(value.begin(), value.end(), matches, regex))
+        return false;
+
+    hour_type hour = from_decimal_submatch<hour_type>(matches[4]);
+    min_type min = from_decimal_submatch<min_type>(matches[5]);
+    sec_type sec = from_decimal_submatch<sec_type>(matches[6]);
+
+    if (hour > 23 || min > 59 || sec > 60)
+        return false;
+
+    datetime = ptime(date(from_decimal_submatch<year_type>(matches[3]),
+                          from_submatch_to_month<month_type>(matches[2]),
+                          from_decimal_submatch<day_type>(matches[1])),
+                     time_duration(hour, min, sec));
+    return true;
+}
+
+template<class String>
+bool rfc1036(const String &value, posix_time::ptime &datetime)
+{
+    using namespace gregorian;
+    using namespace posix_time;
+
+    typedef date::year_type::value_type year_type;
+    typedef date::month_type::value_type month_type;
+    typedef date::day_type::value_type day_type;
+    typedef time_duration::hour_type hour_type;
+    typedef time_duration::min_type min_type;
+    typedef time_duration::sec_type sec_type;
+
+    static const std::basic_regex<typename String::value_type>
+        regex("(?:Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday), " // day
+              "(\\d{2})-" // day-1
+              "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-" // month-2
+              "(\\d{2}) " // year-3
+              "(\\d{2}):" // hour-4
+              "(\\d{2}):" // minutes-5
+              "(\\d{2}) " // seconds-6
+              "GMT");
+
+    std::match_results<typename String::const_iterator> matches;
+    if (!std::regex_match(value.begin(), value.end(), matches, regex))
+        return false;
+
+    hour_type hour = from_decimal_submatch<hour_type>(matches[4]);
+    min_type min = from_decimal_submatch<min_type>(matches[5]);
+    sec_type sec = from_decimal_submatch<sec_type>(matches[6]);
+
+    if (hour > 23 || min > 59 || sec > 60)
+        return false;
+
+    datetime = ptime(date(from_decimal_submatch<year_type>(matches[3]) + 1900,
+                          from_submatch_to_month<month_type>(matches[2]),
+                          from_decimal_submatch<day_type>(matches[1])),
+                     time_duration(hour, min, sec));
+    return true;
+}
+
+template<class String>
+bool asctime(const String &value, posix_time::ptime &datetime)
+{
+    using namespace gregorian;
+    using namespace posix_time;
+
+    typedef date::year_type::value_type year_type;
+    typedef date::month_type::value_type month_type;
+    typedef date::day_type::value_type day_type;
+    typedef time_duration::hour_type hour_type;
+    typedef time_duration::min_type min_type;
+    typedef time_duration::sec_type sec_type;
+
+    static const std::basic_regex<typename String::value_type>
+        regex("(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun) " // day
+              "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) " // month-1
+              "((?:\\d| )\\d) " // day-2
+              "(\\d{2}):" // hour-3
+              "(\\d{2}):" // minutes-4
+              "(\\d{2}) " // seconds-5
+              "(\\d{4})" // year-6
+              );
+
+    std::match_results<typename String::const_iterator> matches;
+    if (!std::regex_match(value.begin(), value.end(), matches, regex))
+        return false;
+
+    hour_type hour = from_decimal_submatch<hour_type>(matches[3]);
+    min_type min = from_decimal_submatch<min_type>(matches[4]);
+    sec_type sec = from_decimal_submatch<sec_type>(matches[5]);
+
+    if (hour > 23 || min > 59 || sec > 60)
+        return false;
+
+    datetime = ptime(date(from_decimal_submatch<year_type>(matches[6]),
+                          from_submatch_to_month<month_type>(matches[1]),
+                          [&matches]() {
+                              using detail::from_decimal_string;
+                              auto m = matches[2];
+                              if (*m.first == ' ')
+                                  ++m.first;
+                              return from_decimal_string<day_type>(m.first,
+                                                                   m.second);
+                          }()),
+                     time_duration(hour, min, sec));
+    return true;
+}
+
+} // namespace detail
+
+/**
+ * Use is_not_a_date_time() to check if the conversion failed.
+ */
+template<class String>
+posix_time::ptime header_to_ptime(const String &value)
+{
+    using namespace gregorian;
+    using namespace posix_time;
+    ptime ret(date_time::not_a_date_time);
+    if (!detail::rfc1123(value, ret)) {
+        if (!detail::rfc1036(value, ret))
+            detail::asctime(value, ret);
+    }
+    return ret;
+}
 
 /**
  * \p's signature MUST be:
---
+++ src/tests/datetime.cpp
@@ -0,0 +1,168 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include <boost/http/algorithm/header.hpp>
+
+#include <iostream>
+
+template<class Target, class String>
+Target from_decimal_string(const String &value)
+{
+    return boost::http::detail::from_decimal_string<Target>(value.begin(),
+                                                            value.end());
+}
+
+template<class Target, unsigned N>
+Target from_decimal_string(const char (&value)[N])
+{
+    return boost::http::detail::from_decimal_string<Target>(value,
+                                                            value + N - 1);
+}
+
+template<class Y, class Mo, class D, class H, class Mi, class S>
+boost::posix_time::ptime make_datetime(Y y, Mo mo, D d, H h, Mi mi, S s)
+{
+    using namespace boost::posix_time;
+    return ptime(boost::gregorian::date(y, mo, d), time_duration(h, mi, s));
+}
+
+void reset(boost::posix_time::ptime &datetime)
+{
+    datetime = boost::posix_time::ptime();
+}
+
+BOOST_AUTO_TEST_CASE(from_decimal_string_case) {
+    // Just need to test up to 4 digits
+    BOOST_CHECK(from_decimal_string<int>("") == 0);
+    BOOST_CHECK(from_decimal_string<int>("0") == 0);
+    BOOST_CHECK(from_decimal_string<int>("000") == 0);
+    BOOST_CHECK(from_decimal_string<int>("001") == 1);
+    BOOST_CHECK(from_decimal_string<int>("1") == 1);
+    BOOST_CHECK(from_decimal_string<int>("10") == 10);
+    BOOST_CHECK(from_decimal_string<int>("010") == 10);
+    BOOST_CHECK(from_decimal_string<int>("0010") == 10);
+    BOOST_CHECK(from_decimal_string<int>("11") == 11);
+    BOOST_CHECK(from_decimal_string<int>("011") == 11);
+    BOOST_CHECK(from_decimal_string<int>("0011") == 11);
+    BOOST_CHECK(from_decimal_string<int>("1000") == 1000);
+    BOOST_CHECK(from_decimal_string<int>("2020") == 2020);
+    BOOST_CHECK(from_decimal_string<int>("1984") == 1984);
+    BOOST_CHECK(from_decimal_string<int>("1337") == 1337);
+    BOOST_CHECK(from_decimal_string<int>("42") == 42);
+}
+
+BOOST_AUTO_TEST_CASE(rfc1123_rfc1036_asctime) {
+    using namespace boost::posix_time;
+    using boost::http::detail::rfc1123;
+    using boost::http::detail::rfc1036;
+    using boost::http::detail::asctime;
+    using boost::string_ref;
+
+    ptime datetime;
+
+    // "additive" (correct inputs are correct outputs) tests
+    BOOST_REQUIRE(rfc1123(string_ref("Sun, 06 Nov 1994 08:49:37 GMT"),
+                          datetime));
+    BOOST_REQUIRE(datetime == make_datetime(1994, 11, 6, 8, 49, 37));
+
+    reset(datetime);
+    BOOST_REQUIRE(datetime == ptime());
+
+    BOOST_REQUIRE(rfc1036(string_ref("Sunday, 06-Nov-94 08:49:37 GMT"),
+                          datetime));
+    BOOST_REQUIRE(datetime == make_datetime(1994, 11, 6, 8, 49, 37));
+
+    reset(datetime);
+
+    BOOST_REQUIRE(asctime(string_ref("Sun Nov  6 08:49:37 1994"), datetime));
+    BOOST_REQUIRE(datetime == make_datetime(1994, 11, 6, 8, 49, 37));
+
+    BOOST_REQUIRE(rfc1123(string_ref("Tue, 01 Dec 1903 00:00:00 GMT"),
+                          datetime));
+    BOOST_REQUIRE(datetime == make_datetime(1903, 12, 1, 0, 0, 0));
+
+    reset(datetime);
+
+    BOOST_REQUIRE(rfc1036(string_ref("Tuesday, 01-Dec-03 00:00:00 GMT"),
+                          datetime));
+    BOOST_REQUIRE(datetime == make_datetime(1903, 12, 1, 0, 0, 0));
+
+    reset(datetime);
+
+    BOOST_REQUIRE(asctime(string_ref("Tue Dec  1 00:00:00 1903"),
+                          datetime));
+    BOOST_REQUIRE(datetime == make_datetime(1903, 12, 1, 0, 0, 0));
+
+    // "negative" (invalid input is rejected) tests
+    BOOST_CHECK(!rfc1123(string_ref("Sun, 06 Nov 1994 24:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1123(string_ref("Sun, 06 Nov 1994 08:60:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1123(string_ref("Sun, 06 Nov 1994 08:49:61 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1123(string_ref("Sun, 06 Nov 1994 08:49:37 GMT"
+                                    "Sun, 06 Nov 1994 08:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1123(string_ref("Sunday, 06-Nov-94 08:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1123(string_ref("Sun Nov  6 08:49:37 1994"), datetime));
+    BOOST_CHECK(!rfc1123(string_ref("All your base are belong to us"),
+                         datetime));
+
+    BOOST_CHECK(!rfc1036(string_ref("Sunday, 06-Nov-94 24:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1036(string_ref("Sunday, 06-Nov-94 08:60:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1036(string_ref("Sunday, 06-Nov-94 08:49:61 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1036(string_ref("Sunday, 06-Nov-94 08:49:37 GMT"
+                                    "Sunday, 06-Nov-94 08:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1036(string_ref("Sun, 06 Nov 1994 08:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!rfc1036(string_ref("Sun Nov  6 08:49:37 1994"), datetime));
+    BOOST_CHECK(!rfc1036(string_ref("All your base are belong to us"),
+                         datetime));
+
+    BOOST_CHECK(!asctime(string_ref("Tue Dec  1 24:00:00 1903"),
+                          datetime));
+    BOOST_CHECK(!asctime(string_ref("Tue Dec  1 00:60:00 1903"),
+                          datetime));
+    BOOST_CHECK(!asctime(string_ref("Tue Dec  1 00:00:61 1903"),
+                          datetime));
+    BOOST_CHECK(!asctime(string_ref("Tue Dec  1 00:00:00 1903"
+                                    "Tue Dec  1 00:00:00 1903"),
+                          datetime));
+    BOOST_CHECK(!asctime(string_ref("Sun, 06 Nov 1994 08:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!asctime(string_ref("Sunday, 06-Nov-94 08:49:37 GMT"),
+                         datetime));
+    BOOST_CHECK(!asctime(string_ref("All your base are belong to us"),
+                         datetime));
+}
+
+BOOST_AUTO_TEST_CASE(header_to_ptime_case) {
+    using namespace boost::posix_time;
+    using boost::http::header_to_ptime;
+    using boost::string_ref;
+
+    ptime datetime;
+
+    BOOST_CHECK(header_to_ptime(string_ref("Sun, 06 Nov 1994 08:49:37 GMT"))
+                == make_datetime(1994, 11, 6, 8, 49, 37));
+
+    reset(datetime);
+
+    BOOST_REQUIRE(datetime == ptime());
+
+    BOOST_CHECK(header_to_ptime(string_ref("Sunday, 06-Nov-94 08:49:37 GMT"))
+                == make_datetime(1994, 11, 6, 8, 49, 37));
+
+    reset(datetime);
+
+    BOOST_CHECK(header_to_ptime(string_ref("Sun Nov  6 08:49:37 1994"))
+                == make_datetime(1994, 11, 6, 8, 49, 37));
+
+    BOOST_CHECK(header_to_ptime(string_ref("All your base are belong to us"))
+                .is_not_a_date_time());
+}
