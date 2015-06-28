eb03764f73271de02979637f4e4315eccfc2c98d
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2015-06-28 14:49:21 -0300

    refactor: using using-declaration to largely simplify buffered_socket
    Using-declaration introduces a member of a base class into the derived
    class definition. Lots of the trivial wrapper functions could be exposed
    by just pulling them from Parent.
    
    Thanks to Bjorn for the hint:
    https://github.com/BoostGSoC14/boost.http/commit/2b2e7ecebd27bfca6ce7b1ad84faec39998fc561#commitcomment-11795501

--- include/boost/http/buffered_socket.hpp
+++ include/boost/http/buffered_socket.hpp
@@ -18,129 +18,26 @@ namespace http {
 template<class Socket, std::size_t N = BOOST_HTTP_SOCKET_DEFAULT_BUFFER_SIZE>
 class basic_buffered_socket: private ::boost::http::basic_socket<Socket>
 {
+    typedef ::boost::http::basic_socket<Socket> Parent;
+
 public:
     static_assert(N > 0, "N must be greater than 0");
 
     typedef Socket next_layer_type;
 
-    http::read_state read_state() const
-    {
-        return Parent::read_state();
-    }
-
-    http::write_state write_state() const
-    {
-        return Parent::write_state();
-    }
-
-    bool write_response_native_stream() const
-    {
-        return Parent::write_response_native_stream();
-    }
-
-    asio::io_service &get_io_service()
-    {
-        return Parent::get_io_service();
-    }
-
-    template<class String, class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_read_request(String &method, String &path, Message &message,
-                       CompletionToken &&token)
-    {
-        return Parent::async_read_request(method, path, message,
-                                          std::forward<CompletionToken>(token));
-    }
-
-    template<class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_read_some(Message &message, CompletionToken &&token)
-    {
-        return Parent::async_read_some(message,
-                                       std::forward<CompletionToken>(token));
-    }
-
-    template<class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_read_trailers(Message &message, CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_read_trailers(message, std::forward<CT>(token));
-    }
-
-    template<class StringRef, class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_write_response(std::uint_fast16_t status_code,
-                         const StringRef &reason_phrase, const Message &message,
-                         CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_write_response(status_code, reason_phrase, message,
-                                            std::forward<CT>(token));
-    }
-
-    template<class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_write_response_continue(CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_write_response_continue(std::forward<CT>(token));
-    }
-
-    template<class StringRef, class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_write_response_metadata(std::uint_fast16_t status_code,
-                                  const StringRef &reason_phrase,
-                                  const Message &message,
-                                  CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_write_response_metadata(status_code, reason_phrase,
-                                                     message,
-                                                     std::forward<CT>(token));
-    }
-
-    template<class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_write(const Message &message, CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_write(message, std::forward<CT>(token));
-    }
-
-    template<class Message, class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_write_trailers(const Message &message, CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_write_trailers(message, std::forward<CT>(token));
-    }
-
-    template<class CompletionToken>
-    typename asio::async_result<
-        typename asio::handler_type<CompletionToken,
-                                    void(system::error_code)>::type>::type
-    async_write_end_of_message(CompletionToken &&token)
-    {
-        typedef CompletionToken CT;
-        return Parent::async_write_end_of_message(std::forward<CT>(token));
-    }
+    using Parent::read_state;
+    using Parent::write_state;
+    using Parent::write_response_native_stream;
+    using Parent::get_io_service;
+    using Parent::async_read_request;
+    using Parent::async_read_some;
+    using Parent::async_read_trailers;
+    using Parent::async_write_response;
+    using Parent::async_write_response_continue;
+    using Parent::async_write_response_metadata;
+    using Parent::async_write;
+    using Parent::async_write_trailers;
+    using Parent::async_write_end_of_message;
 
     basic_buffered_socket(boost::asio::io_service &io_service)
         : Parent(io_service, boost::asio::buffer(buffer))
@@ -151,19 +48,9 @@ public:
         : Parent(boost::asio::buffer(buffer), std::forward<Args>(args)...)
     {}
 
-    next_layer_type &next_layer()
-    {
-        return Parent::next_layer();
-    }
-
-    const next_layer_type &next_layer() const
-    {
-        return Parent::next_layer();
-    }
+    using Parent::next_layer;
 
 private:
-    typedef ::boost::http::basic_socket<Socket> Parent;
-
     char buffer[N];
 };
 
