582f7da14400907a747ac3243ba5cd731072f7c2
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2015-07-19 18:37:51 -0300

    update spawn example
    Now the spawn example will correctly handle several concurrent
    connections.

--- example/spawn.cpp
+++ example/spawn.cpp
@@ -10,43 +10,39 @@
 using namespace std;
 using namespace boost;
 
-int main()
+class connection: public std::enable_shared_from_this<connection>
 {
-    asio::io_service ios;
-
-    asio::ip::tcp::acceptor acceptor(ios, asio::ip::tcp
-                                     ::endpoint(asio::ip::tcp::v6(), 8080));
-
-    auto work = [&acceptor](asio::yield_context yield) {
-        while (true) {
-            http::buffered_socket socket(acceptor.get_io_service());
-            try {
-                std::string method;
-                std::string path;
-                http::message message;
-
-                cout << "About to accept a new connection" << endl;
-                acceptor.async_accept(socket.next_layer(), yield);
-
-                cout << "About to receive a new message" << endl;
+public:
+    void operator()(asio::yield_context yield)
+    {
+        auto guard = shared_from_this();
+        try {
+            while (true) {
+                cout << '[' << counter << "] About to receive a new message"
+                     << endl;
                 socket.async_read_request(method, path, message, yield);
                 //message.body().clear(); // freeing not used resources
 
                 if (http::request_continue_required(message)) {
-                    cout << "Continue required. About to send \"100-continue\""
+                    cout << '[' << counter
+                         << "] Continue required. About to send"
+                        " \"100-continue\""
                          << std::endl;
                     socket.async_write_response_continue(yield);
                 }
 
                 while (socket.read_state() != http::read_state::empty) {
-                    cout << "Message not fully received" << endl;
+                    cout << '[' << counter << "] Message not fully received"
+                         << endl;
                     switch (socket.read_state()) {
                     case http::read_state::message_ready:
-                        cout << "About to receive some body" << endl;
+                        cout << '[' << counter << "] About to receive some body"
+                             << endl;
                         socket.async_read_some(message, yield);
                         break;
                     case http::read_state::body_ready:
-                        cout << "About to receive trailers" << endl;
+                        cout << '[' << counter << "] About to receive trailers"
+                             << endl;
                         socket.async_read_trailers(message, yield);
                         break;
                     default:;
@@ -59,44 +55,103 @@ int main()
                 //}
                 //cout << "==" << endl;
 
-                cout << "Message received. State = "
+                cout << '[' << counter << "] Message received. State = "
                      << int(socket.read_state()) << endl;
-                cout << "Method: " << method << endl;
-                cout << "Path: " << path << endl;
+                cout << '[' << counter << "] Method: " << method << endl;
+                cout << '[' << counter << "] Path: " << path << endl;
                 {
                     auto host = message.headers().find("host");
                     if (host != message.headers().end())
-                        cout << "Host header: " << host->second << endl;
+                        cout << '[' << counter << "] Host header: "
+                             << host->second << endl;
                 }
 
-                std::cout << "Write state = " << int(socket.write_state())
-                << std::endl;
+                std::cout << '[' << counter << "] Write state = "
+                          << int(socket.write_state()) << std::endl;
 
-                cout << "About to send a reply" << endl;
+                cout << '[' << counter << "] About to send a reply" << endl;
 
                 http::message reply;
-                reply.headers().emplace("connection", "close");
-                const char body[] = "Foobar";
+                //reply.headers().emplace("connection", "close");
+                const char body[] = "Hello World\n";
                 std::copy(body, body + sizeof(body) - 1,
                           std::back_inserter(reply.body()));
 
                 socket.async_write_response(200, string_ref("OK"), reply,
                                             yield);
-            } catch (system::system_error &e) {
-                if (e.code()
-                    != system::error_code{http::http_errc::stream_finished}) {
-                    cerr << "Aborting on exception: " << e.what() << endl;
-                    std::exit(1);
-                }
+            }
+        } catch (system::system_error &e) {
+            if (e.code()
+                != system::error_code{http::http_errc::stream_finished}) {
+                cerr << '[' << counter << "] Aborting on exception: "
+                     << e.what() << endl;
+                std::exit(1);
+            } else {
+                cout << '[' << counter << "] Closing connection" << endl;
+            }
 
-                socket.next_layer()
-                .shutdown(asio::ip::tcp::socket::shutdown_both);
-                socket.next_layer().close();
+            socket.next_layer()
+            .shutdown(asio::ip::tcp::socket::shutdown_both);
+            socket.next_layer().close();
+        } catch (std::exception &e) {
+            cerr << '[' << counter << "] Aborting on exception: " << e.what()
+                 << endl;
+            std::exit(1);
+        }
+    }
+
+    asio::ip::tcp::socket &tcp_layer()
+    {
+        return socket.next_layer();
+    }
+
+    static std::shared_ptr<connection> make_connection(asio::io_service &ios,
+                                                       int counter)
+    {
+        return std::shared_ptr<connection>{new connection{ios, counter}};
+    }
+
+private:
+    connection(asio::io_service &ios, int counter)
+        : socket(ios)
+        , counter(counter)
+    {}
+
+    http::buffered_socket socket;
+    int counter;
+
+    std::string method;
+    std::string path;
+    http::message message;
+};
+
+int main()
+{
+    asio::io_service ios;
+    asio::ip::tcp::acceptor acceptor(ios,
+                                     asio::ip::tcp
+                                     ::endpoint(asio::ip::tcp::v6(), 8080));
+
+    auto work = [&acceptor](asio::yield_context yield) {
+        int counter = 0;
+        for ( ; true ; ++counter ) {
+            try {
+                auto connection
+                    = connection::make_connection(acceptor.get_io_service(),
+                                                  counter);
+                cout << "About to accept a new connection" << endl;
+                acceptor.async_accept(connection->tcp_layer(), yield);
+
+                auto handle_connection
+                    = [connection](asio::yield_context yield) mutable {
+                    (*connection)(yield);
+                };
+                spawn(acceptor.get_io_service(), handle_connection);
             } catch (std::exception &e) {
                 cerr << "Aborting on exception: " << e.what() << endl;
                 std::exit(1);
             }
-        };
+        }
     };
 
     cout << "About to spawn" << endl;
