ccf5e29dd4666102ac0d2c653160ccea6d4fa877
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2016-08-18 06:52:41 -0300

    Add failing test to POST with "Content-Length: 0"

--- test/request.cpp
+++ test/request.cpp
@@ -5720,3 +5720,130 @@ TEST_CASE("Parse a few (good,bad) pipelined non-fragmented/whole requests",
         REQUIRE(parser.code() == http::token::code::error_invalid_data);
     }
 }
+
+TEST_CASE("Parse a single message with no body", "[parser,good]")
+{
+    http::reader::request parser;
+
+    REQUIRE(parser.code() == http::token::code::error_insufficient_data);
+    REQUIRE(parser.token_size() == 0);
+    REQUIRE(parser.expected_token() == http::token::code::method);
+
+    /* DO NOT increase buffer length. This test is intended to make sure parser
+       doesn't stay stuck after headers waiting for some byte before emitting
+       delimiters. */
+    parser.set_buffer(my_buffer("POST /upload HTTP/1.1\r\n"
+                                "Content-length: 0\r\n"
+                                "host:thelastringbearer.org\r\n"
+                                "\r\n"));
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::method);
+    REQUIRE(parser.token_size() == 4);
+    REQUIRE(parser.value<http::token::method>() == "POST");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.expected_token() == http::token::code::request_target);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::request_target);
+    REQUIRE(parser.token_size() == 7);
+    REQUIRE(parser.value<http::token::request_target>() == "/upload");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 8);
+    REQUIRE(parser.expected_token() == http::token::code::version);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::version);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.value<http::token::version>() == 1);
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_name);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::field_name);
+    REQUIRE(parser.token_size() == 14);
+    REQUIRE(parser.value<http::token::field_name>() == "Content-length");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_value);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::field_value);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.value<http::token::field_value>() == "0");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_name);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::field_name);
+    REQUIRE(parser.token_size() == 4);
+    REQUIRE(parser.value<http::token::field_name>() == "host");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.expected_token() == http::token::code::field_value);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::field_value);
+    REQUIRE(parser.token_size() == 21);
+    REQUIRE(parser.value<http::token::field_value>()
+            == "thelastringbearer.org");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_name);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::end_of_headers);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::end_of_body);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::end_of_body);
+    REQUIRE(parser.token_size() == 0);
+    REQUIRE(parser.expected_token() == http::token::code::end_of_message);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::end_of_message);
+    REQUIRE(parser.token_size() == 0);
+    REQUIRE(parser.expected_token() == http::token::code::method);
+}
