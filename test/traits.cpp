eebc61986526f996ee89ee3906d783b347aa33b7
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2018-03-24 04:28:19 -0300

    Remove all runtime-based polymorphic helpers.
    These helpers type erased the callbacks and didn't store the associated
    executor or allocator. Therefore, they were of limited use and quite
    possibly dangerous use.
    
    I need to give some thought on how associated executors and allocators
    could be preserved, but it's not really easy.

--- test/traits.cpp
+++ test/traits.cpp
@@ -1,7 +1,6 @@
 #include <boost/http/request.hpp>
 #include <boost/http/response.hpp>
 #include <boost/http/buffered_socket.hpp>
-#include <boost/http/server_socket_adaptor.hpp>
 
 using namespace boost;
 using namespace std;
@@ -87,35 +86,5 @@ static_assert(http::is_socket<http::buffered_socket>::value,
               "http::buffered_socket is not a socket?!");
 static_assert(http::is_socket<http::basic_buffered_socket<int>>::value,
               "http::basic_buffered_socket<int> is not a socket?!");
-
-static_assert(!http::is_server_socket<http::polymorphic_socket_base>::value,
-              "http::polymorphic_socket_base is a server_socket?!");
-static_assert(!http::is_server_socket<http::basic_polymorphic_socket_base<int>>
-              ::value,
-              "http::basic_polymorphic_socket_base<int> is a server_socket?!");
-
-static_assert(http::is_socket<http::polymorphic_socket_base>::value,
-              "http::polymorphic_socket_base is not a socket?!");
-static_assert(http::is_socket<http::basic_polymorphic_socket_base<int>>::value,
-              "http::basic_polymorphic_socket_base<int> is not a socket?!");
-
-static_assert(http::is_server_socket<http::polymorphic_server_socket>::value,
-              "http::polymorphic_server_socket is a server_socket?!");
-static_assert(http::is_server_socket<http::basic_polymorphic_server_socket<int,
-                                                                           int>>
-              ::value,
-              "http::basic_polymorphic_server_socket<int> is not a"
-              " server_socket?!");
-
-static_assert(http::is_socket<http::polymorphic_server_socket>::value,
-              "http::polymorphic_server_socket is not a socket?!");
-static_assert(http::is_socket<http::basic_polymorphic_server_socket<int, int>>
-              ::value,
-              "http::basic_polymorphic_server_socket<int> is not a socket?!");
-
-static_assert(http::is_server_socket<http::server_socket_adaptor<int>>::value,
-              "http::server_socket_adaptor<int> is not a server_socket?!");
-static_assert(http::is_socket<http::server_socket_adaptor<int>>::value,
-              "http::server_socket_adaptor<int> is not a socket?!");
 
 int main() {}
