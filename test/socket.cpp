2179341fc1a446f2ca948a2756ef6642f7868f6b
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2018-10-20 22:57:32 -0300

    Move fiber_baton util out of test/socket.cpp

--- test/socket.cpp
+++ test/socket.cpp
@@ -12,6 +12,7 @@
 #include <boost/http/request.hpp>
 #include <boost/http/response.hpp>
 
+#include "utils/fiber_baton.hpp"
 #include "utils/mocksocket.hpp"
 
 using namespace boost;
@@ -55,52 +56,6 @@ bool check_wrong_direction(const system::system_error &e)
 {
     return system::error_code(http::http_errc::wrong_direction) == e.code();
 }
-
-// inspired by
-// https://github.com/facebook/folly/blob/master/folly/fibers/Baton.h
-// (it's just a 1:1 semaphore)
-class fiber_baton
-{
-public:
-    fiber_baton(boost::asio::io_context &ctx)
-        : executor(ctx.get_executor())
-    {}
-
-    fiber_baton(const fiber_baton&) = delete;
-
-    void post()
-    {
-        if (!pending) {
-            posted = true;
-            return;
-        }
-
-        std::allocator<void> alloc;
-        auto p = std::move(pending);
-        pending = nullptr;
-        executor.dispatch(std::move(p), alloc);
-    }
-
-    void wait(asio::yield_context &yield)
-    {
-        if (posted) {
-            posted = false;
-            return;
-        }
-
-        // suspend
-        boost::asio::async_completion<
-            decltype(yield), void(boost::system::error_code)
-        > init{yield};
-        pending = std::move(init.completion_handler);
-        init.result.get();
-    }
-
-private:
-    boost::asio::io_context::executor_type executor;
-    bool posted = false;
-    std::function<void()> pending;
-};
 
 BOOST_AUTO_TEST_CASE(socket_ctor) {
     bool captured = false;
---
+++ test/utils/fiber_baton.hpp
@@ -0,0 +1,47 @@
+#include <boost/asio/spawn.hpp>
+
+// inspired by
+// https://github.com/facebook/folly/blob/master/folly/fibers/Baton.h
+// (it's just a 1:1 semaphore)
+class fiber_baton
+{
+public:
+    fiber_baton(boost::asio::io_context &ctx)
+        : executor(ctx.get_executor())
+    {}
+
+    fiber_baton(const fiber_baton&) = delete;
+
+    void post()
+    {
+        if (!pending) {
+            posted = true;
+            return;
+        }
+
+        std::allocator<void> alloc;
+        auto p = std::move(pending);
+        pending = nullptr;
+        executor.dispatch(std::move(p), alloc);
+    }
+
+    void wait(boost::asio::yield_context &yield)
+    {
+        if (posted) {
+            posted = false;
+            return;
+        }
+
+        // suspend
+        boost::asio::async_completion<
+            decltype(yield), void(boost::system::error_code)
+        > init{yield};
+        pending = std::move(init.completion_handler);
+        init.result.get();
+    }
+
+private:
+    boost::asio::io_context::executor_type executor;
+    bool posted = false;
+    std::function<void()> pending;
+};
