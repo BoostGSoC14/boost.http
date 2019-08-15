20cc2c047963792f4a0838b82d6a57ef9fafb62b
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2019-08-15 10:29:26 -0300

    Add "eof before end" test case

--- test/response.cpp
+++ test/response.cpp
@@ -1204,3 +1204,123 @@ TEST_CASE("eof without puteof", "[parser,good]")
 
     REQUIRE(parser.code() == http::token::code::error_use_another_connection);
 }
+
+TEST_CASE("eof before end", "[parser,bad]")
+{
+    http::reader::response parser;
+
+    REQUIRE(parser.code() == http::token::code::error_insufficient_data);
+    REQUIRE(parser.token_size() == 0);
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.set_buffer(my_buffer("HTTP/1.0 200 OK\r\n"
+                                "content-length: 7\r\n"
+                                "\r\n"
+                                "jaquet"));
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 7);
+    REQUIRE(parser.expected_token() == http::token::code::version);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::version);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.value<http::token::version>() == 0);
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.expected_token() == http::token::code::status_code);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::status_code);
+    REQUIRE(parser.token_size() == 3);
+    REQUIRE(parser.value<http::token::status_code>() == 200);
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.set_method("GET");
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.expected_token() == http::token::code::reason_phrase);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::reason_phrase);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.value<http::token::reason_phrase>() == "OK");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_name);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::field_name);
+    REQUIRE(parser.token_size() == 14);
+    REQUIRE(parser.value<http::token::field_name>() == "content-length");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_value);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::field_value);
+    REQUIRE(parser.token_size() == 1);
+    REQUIRE(parser.value<http::token::field_value>() == "7");
+    REQUIRE(parser.expected_token() == http::token::code::skip);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::skip);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::field_name);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::end_of_headers);
+    REQUIRE(parser.token_size() == 2);
+    REQUIRE(parser.expected_token() == http::token::code::body_chunk);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::body_chunk);
+    REQUIRE(parser.token_size() == 6);
+    {
+        asio::const_buffer body = parser.value<http::token::body_chunk>();
+        REQUIRE(body.size() == 6);
+        const char *buf_view = static_cast<const char*>(body.data());
+        REQUIRE(buf_view[0] == 'j');
+        REQUIRE(buf_view[1] == 'a');
+        REQUIRE(buf_view[2] == 'q');
+        REQUIRE(buf_view[3] == 'u');
+        REQUIRE(buf_view[4] == 'e');
+        REQUIRE(buf_view[5] == 't');
+    }
+    REQUIRE(parser.expected_token() == http::token::code::body_chunk);
+
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::error_insufficient_data);
+    REQUIRE(parser.token_size() == 0);
+    REQUIRE(parser.expected_token() == http::token::code::body_chunk);
+
+    parser.puteof();
+    parser.next();
+
+    REQUIRE(parser.code() == http::token::code::error_use_another_connection);
+    REQUIRE(parser.token_size() == 0);
+}
